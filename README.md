7036e668b58626564970a0793c76588ef52a71f2
zigotica <zigotica@gmail.com>
2012-09-22 02:45:35 +0200

    public launch, with example

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 supersizr
 =========
 
+## WHAT
 jQuery plugin to adjust elements font-size to fit max-width of container
+
+## WHY 
+[Fittext](http://fittextjs.com/) and [bigText](http://www.zachleat.com/web/bigtext-makes-text-big/) are very nice scripts but did not work as expected in my environment.
+
+supersizr basically adds a more accurate yet simpler algorithm, but also better encapsulation, a destroy method that resets to previous CSS values (uses jquery data), a debouncer to avoid excessive calculations…
+
+## HOW
+### common use
+Add the jquery and supersizr scripts in your page and call it as in:
+```
+  $("h1 span").supersizr( );
+```
+This example will take any `span` descendant of an `h1` and fit it to `h1` width (reduce or enlarge)
+### options
+You can override default settings:
+```
+  $("h1 span").supersizr( {"minSize": 20, "maxSize": 200, "preserveLineHeight": 1} );
+```
+minSize and maxSize are quite self-explainatory. 
+preserveLineHeight calculates takes line-height/font-size ratio and applies it to adjust line-height after each element has been fitted.
+
+## Dependencies:
+[jQuery](http://jquery.com/) (should work from v1.7 or newer; only tested on 1.8)
+
+## Todo:
+* check external font (either google/typekit… or font-face) is ready before create/init
+* new setting to keep perceived margin instead of adjusting line-height in linear proportion
+
+## Known issues 
+* Sometimes final width of an element is 1px bigger/smaller than it should. Normally you will use padding on the container, which masks the issue quite well, otherwise this can be a problem. Note: although width is 1px wrong we are using white-space: nowrap for the calculations, thus elements will always fit in one line.
+
+## Complaints?
+Use the issue tracker, send pull requests, the usual polite manners. Thanx.
+
+## License
+Eat it, burn it, use it at your own risk. Or even better, dont use it! ^_^
---
+++ supersizr.js
@@ -0,0 +1,115 @@
+/*
+* supersizr.js 1.0
+* Sergi Meseguer @zigotica
+* https://github.com/zigotica/supersizr
+*/
+
+
+;(function( $ ){
+
+  var count = 0; 
+  // modified from http://remysharp.com/2010/07/21/throttling-function-calls/
+  $.debouncr = function(fn, delay) {
+    var timer = null;
+    return function () {
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, delay || 200);
+    };
+  }
+
+  var methods = {
+    init : function( options ) {  
+      
+      settings = $.extend({
+          'minSize' : 6
+        , 'maxSize' : 1000
+        , 'preserveLineHeight' : 0
+      }, options);
+
+      count++;
+
+      return this.each(function(i){
+
+        var id = $(this).attr("id") || "supersizr_"+ count+i; 
+        $(this).attr("id", id);
+        var elm = $('#'+id);
+
+        elm.supersizr('create', id); 
+        elm.supersizr('sizr', id); 
+
+        // $(window).on('resize.supersizr', function(){elm.supersizr('sizr', id)} );
+        $(window).on('resize.supersizr', $.debouncr( function (event) { elm.supersizr('sizr', id)  }));
+          
+      });
+    }
+
+    , sizr : function ( id ) {
+
+      var elm = $('#'+id);
+
+      // formula / algorithm:
+      var fs = elm.parent().width() / ( elm.outerWidth() / parseInt(elm.css("font-size")) );
+      fs = Math.max(Math.min( fs , parseFloat(settings.maxSize)), parseFloat(settings.minSize));
+
+      var LHratio = 1
+      if( settings.preserveLineHeight == 1) LHratio = elm.data("supersizr").RT;
+
+      elm.css({'font-size': fs, 'line-height': (fs * LHratio) +"px" });
+    }
+
+    , create : function( id ) {
+        var elm = $('#'+id);
+        elm.data('supersizr', {
+            "PW": elm.parent().width()
+          , "DS": elm.css("display")
+          , "WS": elm.css("white-space")
+          , "FS": elm.css("font-size")
+          , "LH": elm.css("line-height")
+          , "RT": parseInt(elm.css("line-height")) / parseInt(elm.css("font-size"))
+        });
+        // we force inline-block and nowrap (needed to calculate width in one line);
+        elm.css({
+          "display": "inline-block", 
+          "white-space": "nowrap"
+        });
+    }
+
+    , destroy : function( ) {
+
+      return this.each(function(){
+
+        var id = $(this).attr("id")
+        var elm = $('#'+id);
+        $(window).unbind('.supersizr');
+        // reset styles:
+        elm.css({
+          "display": elm.data("supersizr").DS, 
+          "white-space": elm.data("supersizr").WS, 
+          "font-size": elm.data("supersizr").FS, 
+          "line-height": elm.data("supersizr").LH 
+        });
+        elm.removeData('supersizr');
+
+      })
+
+     }
+  };
+
+  $.fn.supersizr = function( method ) {
+    
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.supersizr' );
+    }    
+  
+  };
+
+
+})( jQuery );
